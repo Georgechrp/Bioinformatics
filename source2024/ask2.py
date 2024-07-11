ea6dedbba539faa85f207944cf67b071277d354d
Georgechrp <159652944+Georgechrp@users.noreply.github.com>
2024-07-11 13:01:45 +0300

    Update

--- source2024/ask2.py
+++ source2024/ask2.py
@@ -3,72 +3,80 @@ datasetA = ['AAAATCGATGCTTATGGACTGATTCATTCGTAAACTT', 'TCAAATTCACGCTTATGGTCTCATTT
 
 import numpy as np
 
-def global_alignment(A, B, alpha=2):
-    len_A = len(A)
-    len_B = len(B)
-    
-    # Initialize the scoring and traceback matrices
-    scoring_matrix = np.zeros((len_A + 1, len_B + 1))
-    traceback_matrix = np.zeros((len_A + 1, len_B + 1), dtype='object')
-    
-    # Initialize the first row and column
-    for i in range(1, len_A + 1):
-        scoring_matrix[i][0] = scoring_matrix[i-1][0] - alpha
-        traceback_matrix[i][0] = 'up'
-    
-    for j in range(1, len_B + 1):
-        scoring_matrix[0][j] = scoring_matrix[0][j-1] - alpha
-        traceback_matrix[0][j] = 'left'
-    
-    # Fill the scoring and traceback matrices
-    for i in range(1, len_A + 1):
-        for j in range(1, len_B + 1):
-            match = scoring_matrix[i-1][j-1] + (1 if A[i-1] == B[j-1] else -alpha / 2)
-            delete = scoring_matrix[i-1][j] - alpha
-            insert = scoring_matrix[i][j-1] - alpha
-            
-            max_score = max(match, delete, insert)
-            scoring_matrix[i][j] = max_score
-            
-            if max_score == match:
-                traceback_matrix[i][j] = 'diag'
-            elif max_score == delete:
-                traceback_matrix[i][j] = 'up'
-            else:
-                traceback_matrix[i][j] = 'left'
-    
-    # Traceback to get the alignment
-    align_A = []
-    align_B = []
-    i = len_A
-    j = len_B
-    
-    while i > 0 or j > 0:
-        if traceback_matrix[i][j] == 'diag':
-            align_A.append(A[i-1])
-            align_B.append(B[j-1])
+def global_alignment(A, B, alpha=2):    #20206, 21127 --> a=2
+    table = [[0] * (len(B)+1) for _ in range(len(A)+1)]
+    gap_penalty = - alpha
+    match_score = +1
+    mismatch_penalty = - alpha/2
+
+   # Αρχικοποίηση
+    for i in range(1, len(A) + 1):
+        table[i][0] = i * gap_penalty
+    for j in range(1, len(B) + 1):
+        table[0][j] = j * gap_penalty
+
+    # Υπολογισμός
+    for i in range(1, len(A) + 1):
+        for j in range(1, len(B) + 1):
+            match = table[i-1][j-1] + (1 if A[i-1] == B[j-1] else mismatch_penalty)
+            delete = table[i-1][j] + gap_penalty
+            insert = table[i][j-1] + gap_penalty
+            table[i][j] = max(match, delete, insert)
+    for i in table:
+        print(i)
+
+
+
+     # Ανάκτηση της βέλτιστης στοίχισης
+    align1, align2 = [], []
+    i, j = len(A), len(B)
+    align1.append(A[i-1])
+    align2.append(B[j-1])
+
+    while i > 0 and j > 0:
+        score_current = table[i][j]
+        score_diagonal = table[i-1][j-1]
+        score_up = table[i][j-1]
+        score_left = table[i-1][j]
+
+        min_score = max(score_diagonal, score_up, score_left)
+        if min_score == score_diagonal:
             i -= 1
             j -= 1
-        elif traceback_matrix[i][j] == 'up':
-            align_A.append(A[i-1])
-            align_B.append('-')
-            i -= 1
-        elif traceback_matrix[i][j] == 'left':
-            align_A.append('-')
-            align_B.append(B[j-1])
+            align1.append(A[i-1])
+            align2.append(B[j-1])
+        elif min_score == score_up:
             j -= 1
-    
-    # Reverse the alignments as we built them backwards
-    align_A = align_A[::-1]
-    align_B = align_B[::-1]
-    
-    return ''.join(align_A), ''.join(align_B)
+            align1.append(A[i-1])
+            align2.append(B[j-1])
+        elif min_score == score_left:
+            i -= 1
+            align1.append(A[i-1])
+            align2.append(B[j-1])
+        else:
+            align1.append("-")
+            align2.append("-")
+        
 
-# Example usage
-A = "AAAATCGATGCTTATGGACTGATTCATTCGTAAACTT"
-B = "TCAAATTCACGCTTATGGTCTCATTTATTCTAGAGCG"
-align_A, align_B = global_alignment(A, B, alpha=2)
+        
 
-print("Alignment A:", align_A)
-print("Alignment B:", align_B)
 
+        
+
+    align1.reverse()
+    align2.reverse()
+
+    return ''.join(align1), ''.join(align2)
+
+   
+
+
+
+# Παράδειγμα χρήσης
+seq1 = "GCT"
+seq2 = "AGTAC"
+alpha = 2
+
+alignment1, alignment2 = global_alignment(seq1, seq2, alpha)
+print("Alignment 1:", alignment1)
+print("Alignment 2:", alignment2)
